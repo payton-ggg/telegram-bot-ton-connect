b1cf5f1a625638c838e19f3bbd66adfd386a71c5
Marynych Platon <platonmarynych@gmail.com>
2025-04-14 21:00:26 +0300

    asaf

---
+++ src/main.ts
@@ -0,0 +1,67 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { bot } from './bot';
+import { walletMenuCallbacks } from './connect-wallet-menu';
+import {
+    handleConnectCommand,
+    handleDisconnectCommand,
+    handleSendTXCommand,
+    handleShowMyWalletCommand,
+    handleShowTokensCommand
+} from './commands-handlers';
+import { initRedisClient } from './ton-connect/storage';
+import TelegramBot from 'node-telegram-bot-api';
+
+async function main(): Promise<void> {
+    await initRedisClient();
+
+    const callbacks = {
+        ...walletMenuCallbacks
+    };
+
+    bot.on('callback_query', query => {
+        if (!query.data) {
+            return;
+        }
+
+        let request: { method: string; data: string };
+
+        try {
+            request = JSON.parse(query.data);
+        } catch {
+            return;
+        }
+
+        if (!callbacks[request.method as keyof typeof callbacks]) {
+            return;
+        }
+
+        callbacks[request.method as keyof typeof callbacks](query, request.data);
+    });
+
+    bot.onText(/\/connect/, handleConnectCommand);
+
+    bot.onText(/\/send_tx/, handleSendTXCommand);
+
+    bot.onText(/\/disconnect/, handleDisconnectCommand);
+
+    bot.onText(/\/my_wallet/, handleShowMyWalletCommand);
+
+    bot.onText(/\/link/, handleShowTokensCommand);
+
+    bot.onText(/\/start/, (msg: TelegramBot.Message) => {
+        bot.sendMessage(
+            msg.chat.id,
+            `
+This is an example of text
+            
+Commands list: 
+/connect - Connect to a wallet
+/link
+`
+        );
+    });
+}
+
+main();
